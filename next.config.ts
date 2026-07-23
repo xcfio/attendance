02ec85f8531ad47fd9fa855e063ae4abad436f2a
xcfio <119097812+xcfio@users.noreply.github.com>
2026-07-23 20:57:17 +0600

    chore(config): update next.config.ts to include experimental TypeScript CLI option

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,9 @@
 import type { NextConfig } from "next"
 
-const nextConfig: NextConfig = {/* config options here */}
+const nextConfig: NextConfig = {
+    experimental: {
+        useTypeScriptCli: true
+    }
+}
 
 export default nextConfig
